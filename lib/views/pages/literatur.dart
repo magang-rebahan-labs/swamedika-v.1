b65a07063743f19462ee3640281ced3842aee01a
hafidz15miftah <hafidz15miftah@gmail.com>
2022-09-14 23:41:04 +0700

    Adding eBooklist Literatur Page

--- lib/views/pages/literatur.dart
+++ lib/views/pages/literatur.dart
@@ -9,8 +9,7 @@ class Literatur extends StatelessWidget {
         backgroundColor: Colors.white,
         appBar: PreferredSize(
           preferredSize:
-             
-              Size.fromHeight(MediaQuery.of(context).size.height / 4.413),
+              Size.fromHeight(MediaQuery.of(context).size.height / 6.413),
           child: AppBar(
             backgroundColor: Colors.white,
             foregroundColor: Colors.black,
@@ -27,23 +26,408 @@ class Literatur extends StatelessWidget {
           scrollDirection: Axis.vertical,
           child: Column(
             children: [
+              // BUKU SAKU PROBIOTIK
               Padding(
                 padding: const EdgeInsets.only(right: 30, left: 30),
-                child: Row(
+                child:  Row(
                   children: [
-                    Text('1.'),
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '1.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
                     Container(
                       width: MediaQuery.of(context).size.width * 0.2,
                       height: MediaQuery.of(context).size.height * 0.15,
-                      decoration: BoxDecoration(color: Colors.blue),
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-probiotik.png'),
+                              fit: BoxFit.fill)),
                     ),
                     Flexible(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Pedoman Penggunan Herbal & SK Dalam Menghadapi Covid 19'),
-                          Text('eBook')
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Saku Probiotik',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // BUKU SAKU VITAMIN C
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, top: 50),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '2.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-vitaminc.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Saku Vitamin C',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // BUKU SAKU VITAMIN D
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, top: 50),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '3.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-vitamind.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Saku Vitamin D',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // BUKU SAKU VITAMIN E
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, top: 50),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '4.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-probiotik.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Saku Vitamin E',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // BUKU SAKU ZINK
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, top: 50),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '5.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-probiotik.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Saku Zink',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // BUKU SAKU SELENIUM
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, top: 50),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '6.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-probiotik.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Saku Selenium',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // BUKU PEDOMAN PENGGUNAAN HERBAL & SK DALAM MENGHADAPI COVID 19
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, top: 50),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '7.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-probiotik.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Pedoman Penggunaan Herbal & SK Dalam Menghadapi Covid 19',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // BUKU SAKU OBAT TRADISIONAL
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, top: 50, bottom: 50),
+                child: Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: Text(
+                        '8.',
+                        style: TextStyle(
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      decoration: const BoxDecoration(
+                          color: Colors.blue,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/literatur/bs-probiotik.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Buku Saku Obat Tradisional',
+                              style:
+                                  TextStyle(fontSize: 14, fontFamily: 'Poppins'),
+                            ),
+                            Text(
+                              'eBook',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  color: Colors.grey),
+                            )
+                          ],
+                        ),
                       ),
                     )
                   ],
