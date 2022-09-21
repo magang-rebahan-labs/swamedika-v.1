3bd965ca1161f9b3459783de7695dfddc7fbb009
hafidz15miftah <hafidz15miftah@gmail.com>
2022-09-21 13:25:31 +0700

    edit gesturedetector literatur

--- lib/views/pages/literatur.dart
+++ lib/views/pages/literatur.dart
@@ -35,28 +35,28 @@ class Literatur extends StatelessWidget {
               // BUKU PEDOMAN PENGGUNAAN HERBAL & SK DALAM MENGHADAPI COVID 19
               Padding(
                 padding: const EdgeInsets.only(right: 30, left: 30),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(right: 20),
-                      child: Text(
-                        '1.',
-                        style: TextStyle(
-                            fontSize: 17,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.bold),
+                child: GestureDetector(
+                  onTap: () async {
+                    const url = 'https://bit.ly/BUKUPEDOMANHERBALDANSK2020';
+                    if (await launchUrlString(url)) {
+                      await launchUrlString(url);
+                    } else {
+                      throw 'Tidak Dapat membuka $url';
+                    }
+                  },
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: Text(
+                          '1.',
+                          style: TextStyle(
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () async {
-                        const url = 'https://bit.ly/BUKUPEDOMANHERBALDANSK2020';
-                        if (await launchUrlString(url)) {
-                          await launchUrlString(url);
-                        } else {
-                          throw 'Tidak Dapat membuka $url';
-                        }
-                      },
-                      child: Container(
+                      Container(
                         width: MediaQuery.of(context).size.width * 0.2,
                         height: MediaQuery.of(context).size.height * 0.15,
                         decoration: const BoxDecoration(
@@ -66,58 +66,58 @@ class Literatur extends StatelessWidget {
                                     'assets/images/literatur/pedomansup-covid19.png'),
                                 fit: BoxFit.fill)),
                       ),
-                    ),
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              'Buku Pedoman Penggunaan Herbal & SK Dalam Menghadapi Covid 19',
-                              style: TextStyle(
-                                  fontSize: 14, fontFamily: 'Poppins'),
-                            ),
-                            Text(
-                              'eBook',
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  fontFamily: 'Poppins',
-                                  color: Colors.grey),
-                            )
-                          ],
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Buku Pedoman Penggunaan Herbal & SK Dalam Menghadapi Covid 19',
+                                style: TextStyle(
+                                    fontSize: 14, fontFamily: 'Poppins'),
+                              ),
+                              Text(
+                                'eBook',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.grey),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
 
               // BUKU SAKU OBAT TRADISIONAL
               Padding(
                 padding: const EdgeInsets.only(right: 30, left: 30, top: 50),
-                child: Row(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(right: 20),
-                      child: Text(
-                        '2.',
-                        style: TextStyle(
-                            fontSize: 17,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.bold),
+                child: GestureDetector(
+                  onTap: () async {
+                    const url = 'https://bit.ly/BUKUSAKU_OT';
+                    if (await canLaunchUrlString(url)) {
+                      await launchUrlString(url);
+                    } else {
+                      throw 'Tidak Dapat membuka $url';
+                    }
+                  },
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: Text(
+                          '2.',
+                          style: TextStyle(
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () async {
-                        const url = 'https://bit.ly/BUKUSAKU_OT';
-                        if (await canLaunchUrlString(url)) {
-                          await launchUrlString(url);
-                        } else {
-                          throw 'Tidak Dapat membuka $url';
-                        }
-                      },
-                      child: Container(
+                      Container(
                         width: MediaQuery.of(context).size.width * 0.2,
                         height: MediaQuery.of(context).size.height * 0.15,
                         decoration: const BoxDecoration(
@@ -127,30 +127,30 @@ class Literatur extends StatelessWidget {
                                     'assets/images/literatur/bs-obattradisional.png'),
                                 fit: BoxFit.fill)),
                       ),
-                    ),
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 20.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              'Buku Saku Obat Tradisional',
-                              style: TextStyle(
-                                  fontSize: 14, fontFamily: 'Poppins'),
-                            ),
-                            Text(
-                              'eBook',
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  fontFamily: 'Poppins',
-                                  color: Colors.grey),
-                            )
-                          ],
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Buku Saku Obat Tradisional',
+                                style: TextStyle(
+                                    fontSize: 14, fontFamily: 'Poppins'),
+                              ),
+                              Text(
+                                'eBook',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    fontFamily: 'Poppins',
+                                    color: Colors.grey),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
