0e8ddefd34877d5d5b3ad23d56132d967ad2819c
unuStudents <nanda.19572011009@students.unugha.ac.id>
2022-09-22 12:46:31 +0700

    sorting list in category view

--- lib/views/pages/category_views.dart
+++ lib/views/pages/category_views.dart
@@ -26,6 +26,7 @@ class _CategoryViewsState extends State<CategoryViews> {
   }
 
   Widget _buildChild() {
+    _database.sort((a, b) => a.title.compareTo(b.title));
     if (widget.categoryName == "Nabati") {
       return Scaffold(
         backgroundColor: const Color(0xFF12A546),
