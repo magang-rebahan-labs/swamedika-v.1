c2663d3913b608594134d51456c6d829398ca3c7
unuStudents <nanda.19572011009@students.unugha.ac.id>
2022-10-18 11:21:24 +0700

    buang tanda pentung

--- lib/views/pages/detail.dart
+++ lib/views/pages/detail.dart
@@ -21,9 +21,9 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     // Variabel untuk menghitung MediaQuiry Tinggi
     double tinggi = MediaQuery.of(context).size.height;
-    double lebar = MediaQuery.of(context).size.width;
+    // double lebar = MediaQuery.of(context).size.width;
     final dataSource = ModalRoute.of(context)!.settings.arguments as Databases;
-    print(lebar);
+
     return Scaffold(
       body: CustomScrollView(slivers: [
         // Sliver untuk AppBar
@@ -116,7 +116,7 @@ class _DetailPageState extends State<DetailPage> {
 
                           // Content Deskripsi
                           Text(
-                            dataSource.desc!,
+                            dataSource.desc,
                             style: kEveryDataDetail,
                             textAlign: TextAlign.justify,
                           ),
